6b8735f2d4c47bb977a2126e44ff156ff2873910
infosecankit <ankit.singh@khoros.com>
2022-11-08 13:06:49 +0530

    Removed unwanted code

--- comparison.py
+++ comparison.py
@@ -11,10 +11,6 @@ df1 = pd.read_csv("HR.csv")
 df2 = pd.read_csv("IT.csv")
 #print(df2)
 
-#matching_emails = df2[df2["Email"].isin(df1["Email"])]
-#print(matching_emails.shape)
-#print(matching_emails)
-
 #see if the data is not found in the HR/legal sheet but is present in the IT sheet then spit it out which might mean they do not have a account with the HR or legal but have an Active onelogin account
 not_matching_emails = df2[~ df2["Email"].isin(df1["Email"])]
 print(not_matching_emails.shape)
